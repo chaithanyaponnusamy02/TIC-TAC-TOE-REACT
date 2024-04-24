ad408e0f1033744ec75900ab0ec3525f9c6461bb
chaithanyaponnusamy02 <chaithanya2646@gmail.com.com>
2024-04-24 22:55:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      input: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  handleAddTask = () => {
+    const { tasks, input } = this.state;
+    if (input.trim()) {
+      const newTask = {
+        id: Date.now(),
+        text: input,
+        completed: false // This can be used for marking tasks as completed
+      };
+      this.setState({
+        tasks: [...tasks, newTask],
+        input: '' // Clear input field after adding
+      });
+    }
+  }
+
+  handleDeleteTask = (taskId) => {
+    const { tasks } = this.state;
+    this.setState({
+      tasks: tasks.filter(task => task.id !== taskId)
+    });
+  }
+
+  render() {
+    const { tasks, input } = this.state;
+
+    return (
+      <body>
+        
+     
+      <div className="App">
+        <h1>Todo List</h1>
+        <input
+          type="text"
+          value={input}
+          onChange={this.handleInputChange}
+          placeholder="Enter a task"
+        />
+        <button className="add" onClick={this.handleAddTask}>Add Task</button>
+        <ul>
+          {tasks.map(task => (
+            <li key={task.id}>
+              {task.text}
+              <button className="button"onClick={() => this.handleDeleteTask(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      </body>
+    );
+  }
 }
 
 export default App;
